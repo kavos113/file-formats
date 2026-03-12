02a808890ec4474f33f50bc1d3816de7af0d2506
kavos113 <kohei.hadano@gmail.com>
2026-03-12 14:31:31 +0900

    header read

--- img/src/fmt/bmp.rs
+++ img/src/fmt/bmp.rs
@@ -1,9 +1,23 @@
+use crate::reader::Reader;
+
 struct BitmapFileHeader {
     bf_type: u16,
     bf_size: u32,
     bf_reserved1: u16,
     bf_reserved2: u16,
     bf_off_bits: u32,
+}
+
+impl BitmapFileHeader {
+    fn read_from(reader: &mut Reader) -> Self {
+        BitmapFileHeader {
+            bf_type: reader.read_u16(),
+            bf_size: reader.read_u32(),
+            bf_reserved1: reader.read_u16(),
+            bf_reserved2: reader.read_u16(),
+            bf_off_bits: reader.read_u32(),
+        }
+    }
 }
 
 struct BitmapInfoHeader {
@@ -18,6 +32,24 @@ struct BitmapInfoHeader {
     bi_y_pels_per_meter: i32,
     bi_clr_used: u32,
     bi_clr_important: u32,
+}
+
+impl BitmapInfoHeader {
+    fn read_from(reader: &mut Reader) -> Self {
+        BitmapInfoHeader {
+            bi_size: reader.read_u32(),
+            bi_width: reader.read_i32(),
+            bi_height: reader.read_i32(),
+            bi_planes: reader.read_u16(),
+            bi_bit_count: reader.read_u16(),
+            bi_compression: reader.read_u32(),
+            bi_size_image: reader.read_u32(),
+            bi_x_pels_per_meter: reader.read_i32(),
+            bi_y_pels_per_meter: reader.read_i32(),
+            bi_clr_used: reader.read_u32(),
+            bi_clr_important: reader.read_u32(),
+        }
+    }
 }
 
 struct RGBQuad {
