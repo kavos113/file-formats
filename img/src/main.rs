676ebfd46a31a63fce7f059097a4e9d46685688d
kavos113 <kohei.hadano@gmail.com>
2026-04-27 12:02:10 +0900

    fix examnple

--- img/src/main.rs
+++ img/src/main.rs
@@ -1,17 +1,21 @@
 use std::env;
-use img_renderer_rs::ImgRenderer;
+use img_renderer_rs::{Image, ImgRenderer};
 
 pub mod fmt;
 pub mod reader;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    if args.len() < 2 {
-        eprintln!("Usage: {} [image_path]", args[0]);
-        return;
-    }
+    let img = match args.len() {
+        1 => {
+            Image::sample_image(800, 600)
+        }
 
-    let img = fmt::load_image(&args[1]);
+        _ => {
+            fmt::load_image(&args[1])
+        }
+    };
+
     let mut renderer = ImgRenderer::new(100, 100, img.width as i32, img.height as i32, &img);
     renderer.run();
 }
